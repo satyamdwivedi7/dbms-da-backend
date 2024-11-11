555f33caf78ae4b4f47e9bf837b971731380089c
Satyam Dwivedi <122256218+satyam7579@users.noreply.github.com>
2024-11-11 20:09:40 +0530

    added get function

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const routes = require("./pages/api/routes");
 const cors = require("cors");
-require("dotenv").config(); 
+require("dotenv").config();
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
-app.use("/", routes);
 
-app.get("/", (req, res)=>{
-  res.send("Api is running successfully");
-})
-
-app.listen(8000, () => {
-  mongoose.connect(process.env.MONGODB_URI).then(() => {
-    console.log("Connected to the Database successfully");
-  });
+app.get("/", (req, res) => {
+  res.send("API is running successfully");
 });
+
+// Connect to MongoDB (you can also use this directly in the API route file)
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to the Database successfully");
+  })
+  .catch((err) => {
+    console.error("Failed to connect to the database:", err);
+  });
 
 module.exports = app;
